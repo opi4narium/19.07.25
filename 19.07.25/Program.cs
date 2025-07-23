83b2d4311a8c31af1d3b241a7d38b448913bc5c0
opianarium <opianarium@DESKTOP-J35S8GM>
2025-07-24 00:16:01 +0300

    Добавьте файлы проекта.

---
+++ 19.07.25/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите ваше имя: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Введите ваш возраст: ");
+        int age = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите ваш рост в метрах: ");
+        double height = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите ваш пол (М/Ж): ");
+        string genderInput = Console.ReadLine();
+        string gender = genderInput.ToUpper() == "М" ? "Мужской" : "Женский";
+
+        Console.Write("Введите вашу любимую букву: ");
+        char favoriteLetter = char.Parse(Console.ReadLine());
+
+        Console.Write("Введите ваш город проживания: ");
+        string city = Console.ReadLine();
+
+        Console.WriteLine("\n------ АНКЕТА ПОЛЬЗОВАТЕЛЯ ------");
+        Console.WriteLine("Имя: " + name);
+        Console.WriteLine("Возраст: " + age);
+        Console.WriteLine("Рост: " + height + " м");
+        Console.WriteLine("Пол: " + gender);
+        Console.WriteLine("Любимая буква: " + favoriteLetter);
+        Console.WriteLine("Город: " + city);
+    }
+}
